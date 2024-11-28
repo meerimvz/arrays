76c4b6418013c126c600f1107c45d655b7349c30
meerimvz <meerimzulpukarova5@gmail.com>
2024-11-28 11:02:11 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import random
 
 weights = [213, 300, 200]
-locations = [random.randint(0, 8) for _ in range(3)]
+locations = [random.randint(1, 8) for _ in range(3)]
 print("Hello! Your mission is to guess the locations of 3 buried boxes")
 
 while True:
@@ -10,15 +10,15 @@ while True:
 
     for i in range(3):
         while True:
-            guess = input(f"Enter your guess for the location of box{i+1} (0-8):")
+            guess = input(f"Enter your guess for the location of box{i+1} (1-8):")
         
             if guess.isdigit():
                 guess = int(guess)
-                if 0 <= guess <= 8:
+                if 1 <= guess <= 8:
                     guesses.append(guess)
                     break
                 else:
-                    print("Incorrect. Enter a number between 0 and 8")
+                    print("Incorrect. Enter a number between 1 and 8")
     for i in range(3):
         if guesses[i] == locations[i]:
             correct_guesses += 1
@@ -31,4 +31,4 @@ while True:
             print("Correct locations, but the weights is incorrect. Try again")
     else:
         print(f"Wrong guesses. The boxes are moving to new spots. ") 
-        locations = [random.randint(0, 8) for _ in range(3)]
+        locations = [random.randint(1, 8) for _ in range(3)]
